ad4a036e478d317cd754ba59fb400916a827ce91
gabegomez014 <gomezagabriel2@gmail.com>
2021-06-18 23:30:10 -0400

    Finished Player movement for now

---
+++ Assets/The Great Fleece/Game/Scripts/CameraTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour
+{
+    public Transform camToSnapTo;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Camera.main.transform.position = camToSnapTo.position;
+            Camera.main.transform.rotation = camToSnapTo.rotation;
+        }
+    }
+}
---
+++ Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+
+    private NavMeshAgent _agent;
+    private Animator _animator;
+    private Vector3 _target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _animator = transform.GetChild(0).GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(rayOrigin, out hit, 100))
+            {
+                _agent.SetDestination(hit.point);
+                _target = _agent.destination;
+                _animator.SetBool("Walk", true);
+            }
+        }
+
+        float distance = Vector3.Distance(transform.position, _target);
+        if (distance <= 2)
+        {
+            _animator.SetBool("Walk", false);
+        }
+    }
+}
